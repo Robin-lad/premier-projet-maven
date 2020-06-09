0a2c1ed4dc2c609abc35d1b7aae67ddaa6ee7090
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-09 16:17:00 +0200

    a

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -6,12 +6,10 @@ import com.github.lalyos.jfiglet.FigletFont;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       
-        String asciiArt = FigletFont.convertOneLine("Je m'appelle Robin");
-        System.out.println(asciiArt);
-    }
+public class App {
+	public static void main(String[] args) {
+
+		String asciiArt = FigletFont.convertOneLine("Je m'appelle Robin");
+		System.out.println(asciiArt);
+	}
 }

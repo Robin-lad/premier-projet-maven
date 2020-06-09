de1ae2bdd9fc8b42147942fc54bab82a4e1d7645
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-09 17:30:08 +0200

    fin du tp3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,15 +1,17 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) {
 
-		String asciiArt = FigletFont.convertOneLine("Je m'appelle Robin");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
 		System.out.println(asciiArt);
+		
+		String environnement = ResourceBundle.getBundle("application").getString(
+		"environnement");
+		System.out.println("Environnement : " + environnement);
 	}
 }
